de175718584725394b01c34b805e6ec9aa04d36a
Anton Lazarev <alone2904@gmail.com>
2021-10-03 15:15:45 +1200

    Create init_vision.md and test for helloWorld application

--- src/test/kotlin/com/dataart/coreservice/CoreServiceApplicationTests.kt
+++ src/test/kotlin/com/dataart/coreservice/CoreServiceApplicationTests.kt
@@ -1,13 +1,19 @@
 package com.dataart.coreservice
 
+import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.*
+import org.springframework.http.HttpStatus
 
-@SpringBootTest
-class CoreServiceApplicationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CoreServiceApplicationTests(@Autowired val testRest: TestRestTemplate) {
 
     @Test
-    fun contextLoads() {
+    fun coreServiceControllerTest() {
+        val webTestEntity = testRest.getForEntity<String>("/")
+        assertThat(webTestEntity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(webTestEntity.body).contains("Hello world! We are CORE!")
     }
-
 }
